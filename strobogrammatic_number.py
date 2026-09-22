ab65afe7900c83bfafa88b6b8d75539139fa217a
T.Nandhini Reddy <242t605119@ashokacollege.in>
2026-09-22 19:54:58 +0530

    Implement strobogrammatic number checker

---
+++ strobogrammatic_number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isStrobogrammatic(self, num):
+        pairs = {
+            '0': '0',
+            '1': '1',
+            '6': '9',
+            '8': '8',
+            '9': '6'
+        }
+
+        left = 0
+        right = len(num) - 1
+
+        while left <= right:
+            if num[left] not in pairs or pairs[num[left]] != num[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
